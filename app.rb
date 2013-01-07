7193fe85f72d096e3e48b3319db0fb957da6effb
B Strand <strand@bettermistak.es>
2013-01-07 13:37:02 -0800

    setting sass directory

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
-
 require 'compass'
 require 'sinatra'
 require 'yaml'
 require 'sass'
+
+set :views, :sass => 'views/sass', :haml => 'views', :default => 'views'
+
+helpers do # http://www.sinatrarb.com/intro#Looking%20Up%20Template%20Files
+  def find_template(views, name, engine, &block)
+    _, folder = views.detect { |k,v| engine == Tilt[k] }
+    folder ||= views[:default]
+    super(folder, name, engine, &block)
+  end
+end
 
 get '/styles.css' do
   content_type 'text/css', :charset => 'utf-8'
