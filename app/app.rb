f5252d58a019028c32348128e04587662b7b1323
Mel Quark <mel.quark@gmail.com>
2013-05-14 16:09:25 -0700

    routing fix to serve static assets from public directory

--- app/app.rb
+++ app/app.rb
@@ -8,13 +8,11 @@ require_relative 'helpers'
 
 before { load_yaml_into_hashie_variables }
 
+set :static => true
+set :public_folder => 'public'
 set :views, :sass => 'views/sass', :haml => 'views', :default => 'views'
 
 # Routing
-get '/respond.js' do
-  File.read '/public/respond.js'
-end
-
 get '/styles.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :styles
